503a441175fe5e7f28b7cfae77de7220052358be
Ethan-Walker <1169091731@qq.com>
2019-03-01 03:15:35 +0800

    commit

---
+++ src/leetcode2/Q144_PreOrder.java
@@ -0,0 +1,32 @@
+package leetcode2;
+
+import 程序员代码面试指南.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * created by Ethan-Walker on 2019/3/1
+ */
+public class Q144_PreOrder {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayDeque<TreeNode> stack = new ArrayDeque<>();
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        TreeNode p = root;
+        while (true) { // 当所有元素遍历完，从栈中弹出的是  null
+            res.add(p.val);
+            if (p.right != null) stack.push(p.right);
+            if (p.left != null) p = p.left;
+            else {
+                if (!stack.isEmpty()) {
+                    p = stack.pop();
+                } else break;
+            }
+        }
+        return res;
+    }
+}
